6531bc0ccac9eee95d60b5a2baa2d7459906caaa
shlomis2 <shlomis2@gmail.com>
2020-10-05 17:21:12 -0700

    Update sample.php
    The try catch is working only if the CURL failed,
    The problem is that the CURL worked but the business wasn't found and I got error 500.
    Following this answer: https://stackoverflow.com/a/55009597/2195136
    I this it's a better solution.

--- fusion/php/sample.php
+++ fusion/php/sample.php
@@ -74,11 +74,8 @@ function request($host, $path, $url_params = array()) {
             throw new Exception($response, $http_status);
 
         curl_close($curl);
-    } catch(Exception $e) {
-        trigger_error(sprintf(
-            'Curl failed with error #%d: %s',
-            $e->getCode(), $e->getMessage()),
-            E_USER_ERROR);
+    } catch (\Throwable $t) {
+        echo $t->getMessage(), " at ", $t->getFile(), ":", $t->getLine(), "\n";
     }
 
     return $response;
